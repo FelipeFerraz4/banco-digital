05989d2306960968a52ea2ab13008196ab7a33c7
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-04-28 21:34:04 -0300

    creation basic structure according to the Lab Projectof DIO

---
+++ ContaBanco/src/CurrentAccount.java
@@ -0,0 +1,46 @@
+public class CurrentAccount {
+    private String name;
+    private String bankBranch;
+    private int accontNumber;
+    private double bankBalance;
+
+    public CurrentAccount(String name, String bankBranch, int accontNumber, double bankBalance) {
+        this.name = name;
+        this.bankBranch = bankBranch;
+        this.accontNumber = accontNumber;
+        this.bankBalance = bankBalance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBankBranch() {
+        return bankBranch;
+    }
+
+    public void setBankBranch(String bankBranch) {
+        this.bankBranch = bankBranch;
+    }
+
+    public int getAccontNumber() {
+        return accontNumber;
+    }
+
+    public void setAccontNumber(int accontNumber) {
+        this.accontNumber = accontNumber;
+    }
+
+    public double getBankBalance() {
+        return bankBalance;
+    }
+
+    public void setBankBalance(double bankBalance) {
+        this.bankBalance = bankBalance;
+    }
+
+}
