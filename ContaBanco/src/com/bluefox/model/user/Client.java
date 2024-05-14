09515fa36a89a2ca2d7381c48fdc04cf2baa68f3
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-14 11:32:02 -0300

    create structure client

---
+++ ContaBanco/src/com/bluefox/model/user/Client.java
@@ -0,0 +1,31 @@
+package com.bluefox.model.user;
+
+import java.util.List;
+
+import com.bluefox.model.account.Account;
+
+public class Client {
+    private String name;
+    private List<Account> accounts;
+    
+    public Client(String name, List<Account> accounts) {
+        this.name = name;
+        this.accounts = accounts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+    
+
+    
+
+}
